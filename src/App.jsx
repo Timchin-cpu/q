dd84e9a599f23b6949c1fdd8303d18bc634dca31
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-12-19 19:48:47 +0000

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -216,7 +216,7 @@ const handleMainScroll = useCallback(() => {
   if (carouselRef.current) {
     const el = carouselRef.current;
     const scrolled = el.scrollLeft > 0;
-    const nearEnd = el.scrollLeft >= (el.scrollWidth - el.clientWidth - -100);
+    const nearEnd = el.scrollLeft >= (el.scrollWidth - el.clientWidth - -50);
     
     setIsScrolledMain(scrolled);                    // margin-left ВСЕГДА при скролле
     setHasMarginRightMain(scrolled && !nearEnd);    // margin-right ТОЛЬКО не в конце
